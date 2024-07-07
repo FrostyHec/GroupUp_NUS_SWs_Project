a3f1b6a7b3a2aff552e38168047314864d4c9937
dylan <12210211@mail.sustech.edu.cn>
2024-07-07 22:51:00 +0800

    update sql

--- docs/mysql.sql
+++ docs/mysql.sql
@@ -1,73 +1,52 @@
-create table "User"
+create table user_table
 (
-    id       serial
-        primary key,
+    id       serial primary key,
     username varchar not null,
-    gender   varchar not null,
     password varchar not null
 );
 
-alter table "User"
-    owner to postgres;
-
-create table "Survey"
+create table survey
 (
-    id             serial
-        primary key,
-    invitationcode varchar not null
-        unique,
-    state          varchar
-        constraint "Survey_state_check"
-            check ((state)::text = ANY
-                   ((ARRAY ['ready'::character varying, 'released'::character varying, 'closed'::character varying])::text[])),
-    info           json,
+    id             serial primary key,
+    name            varchar,
+    description varchar,
+    status          int,
+    questions           json,
+    personal_info  json,
+    group_restriction json,
     creator_id     integer
-        references "User"
 );
 
-alter table "Survey"
-    owner to postgres;
+create table survey_owner(
+    owner_id int8,
+    survey_id int8,
+    primary key (survey_id,owner_id) -- owner -> user_table.id,  survey_id -> survey.id
+);
+create table survey_member(
+                             member_id int8,
+                             survey_id int8,
+                             primary key (survey_id,member_id)
+);
 
-create table "Query"
+
+create table query
 (
-    id      serial
-        primary key,
-    state   varchar
-        constraint "Query_state_check"
-            check ((state)::text = ANY
-                   ((ARRAY ['edit'::character varying, 'done'::character varying, 'paired'::character varying])::text[])),
-    temp_id integer
-        references "Survey",
-    info    json
+    id      serial primary key,
+    survey_id int8,
+    member_id int8,
+    status   int,
+    personal_info json,
+    questions_answer json
 );
 
-alter table "Query"
-    owner to postgres;
 
-create table "Group"
+create table group_table
 (
-    id        serial
-        primary key,
-    state     varchar,
-    survey_id integer
-        references "Survey"
+    id        serial primary key,
+    survey_id int8
 );
-
-alter table "Group"
-    owner to postgres;
-
-create table "Group_Member"
-(
-    group_id integer not null
-        references "Group"
-            on delete cascade,
-    user_id  integer not null
-        references "User"
-            on delete cascade,
-    primary key (group_id, user_id)
+create table group_member(
+     group_id int8,
+     member_id int8,
+    primary key (group_id, member_id)
 );
-
-alter table "Group_Member"
-    owner to postgres;
-
-

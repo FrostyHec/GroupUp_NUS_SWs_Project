6bf32822ef7a508da6b543d7c962baa6e4e517d1
Frosty <112929809+FrostyHec@users.noreply.github.com>
2024-07-10 00:29:54 +0800

    add test cases

--- project/backend_server/server/src/test/java/com/sustech/groupup/testcode/controller/user/cases/UserControllerOwnTest.java
+++ project/backend_server/server/src/test/java/com/sustech/groupup/testcode/controller/user/cases/UserControllerOwnTest.java
@@ -23,20 +23,36 @@ public class UserControllerOwnTest {
 
     @Autowired
     private UserQueryAPI queryAPI;
-    
+
     @Autowired
     private AuthAPI authAPI;
 
     @Autowired
     private SurveyAPI surveyAPI;
+
     @Test
     public void testNormalQuery() throws Exception {
         var auth = authAPI.templateUserLogin();//create user
         var uid = auth.getId();
-        surveyAPI.successfulCreateTemplate(auth,List.of(uid),List.of());//创建一个survey
-        var res = queryAPI.successfulQueryOwn(uid,auth,-1,1);
-        assert res.size()==1;
-        assert res.getFirst()==1;
+        surveyAPI.successfulCreateTemplate(auth, List.of(uid), List.of());//创建一个survey
+        var res = queryAPI.successfulQueryOwn(uid, auth, -1, 1);
+        assert res.size() == 1;
+        assert res.getFirst() == 1;
+    }
+
+    @Test
+    public void testPageQuery() throws Exception {
+        var auth = authAPI.templateUserLogin();//create user
+        var uid = auth.getId();
+        int pageSize = 5, total = 17;
+        for (int i = 0; i < total; i++) {
+            surveyAPI.successfulCreateTemplate(auth, List.of(uid), List.of());//创建survey
+        }
+        int i = 0;
+        for (; i < total / pageSize; i++) {
+            assert queryAPI.successfulQueryOwn(uid, auth, pageSize, i + 1).size() == pageSize;
+        }
+        assert queryAPI.successfulQueryOwn(uid, auth, pageSize, i + 1).size() == total % pageSize;
     }
 }
 

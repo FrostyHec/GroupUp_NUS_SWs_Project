6c4acb9eab36e09292a9699acabae2368535a967
dylan <12210211@mail.sustech.edu.cn>
2024-07-09 09:36:32 +0800

    survey API finish, only success response, only GET request were tested

--- project/backend_server/server/src/main/java/com/sustech/groupup/controller/grouping/SurveyController.java
+++ project/backend_server/server/src/main/java/com/sustech/groupup/controller/grouping/SurveyController.java
@@ -1,7 +1,12 @@
 package com.sustech.groupup.controller.grouping;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.sustech.groupup.entity.api.SurveyDTO;
+import com.sustech.groupup.entity.converter.SurveyConverter;
+import com.sustech.groupup.entity.db.SurveyEntity;
+import com.sustech.groupup.services.SurveyService;
+import com.sustech.groupup.utils.Response;
+import org.springframework.web.bind.annotation.*;
 
 import com.sustech.groupup.config.Constant;
 
@@ -11,5 +16,45 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping(Constant.API_VERSION + "/survey")
 @RequiredArgsConstructor
 public class SurveyController {
+    private final SurveyService surveyService;
+    private final SurveyConverter surveyConverter;
 
+    @GetMapping("/{number}")
+    public Response getSurveyInfoById (@PathVariable long number) throws JsonProcessingException {
+        var resp = surveyConverter.toDTO(surveyService.getSurveyById(number));
+        return Response.getSuccess("success",resp);
+    }
+
+    @PostMapping()
+    public Response addSurvey (@RequestBody SurveyDTO surveyDTO) {
+        SurveyEntity surveyEntity = surveyConverter.toEntity(surveyDTO);
+        surveyService.insertSurvey(surveyEntity);
+        return Response.getSuccess("success",surveyEntity.getId());
+    }
+
+    @PutMapping("/{id}")
+    public Response updateSurveyById (@PathVariable long id, @RequestBody SurveyDTO surveyDTO) {
+        SurveyEntity surveyEntity = surveyConverter.toEntity(surveyDTO);
+        surveyEntity.setId(id);
+        surveyService.updateSurvey(surveyEntity);
+        return Response.getSuccess("success",surveyDTO);
+    }
+
+    @DeleteMapping("/{id}")
+    public Response deleteSurveyById (@PathVariable long id) {
+        surveyService.deleteSurveyById(id);
+        return Response.getSuccess("success");
+    }
+
+    @PostMapping("/{id}/status")
+    public Response updateSurveyStatusById (@PathVariable long id, @RequestParam int status) {
+        surveyService.updateStatusBySurveyId(id,status);
+        return Response.getSuccess("success","");
+    }
+
+    @GetMapping("/{id}/status")
+    public Response getSurveyStatusById(@PathVariable long id) {
+        SurveyEntity surveyEntity = surveyService.getSurveyById(id);
+        return Response.getSuccess("success",surveyEntity.getStatus());
+    }
 }
--- project/backend_server/server/src/main/java/com/sustech/groupup/mapper/SurveyMapper.java
+++ project/backend_server/server/src/main/java/com/sustech/groupup/mapper/SurveyMapper.java
@@ -1,19 +1,23 @@
 package com.sustech.groupup.mapper;
 
-//public interface SurveyMapper {
-//    @Select("SELECT * FROM \"Survey\" WHERE id = #{id}")
-//    SurveyEntity getSurveyById(int id);
-//
-//    @Select("SELECT * FROM \"Survey\"")
-//    List<SurveyEntity> getAllSurveys();
-//
-//    @Insert("INSERT INTO \"Survey\"(invitationCode, state, info, creator_id) VALUES(#{invitationCode}, #{state}, #{info}, #{creatorId})")
-//    @Options(useGeneratedKeys = true, keyProperty = "id")
-//    void insertSurvey(SurveyEntity survey);
-//
-//    @Update("UPDATE \"Survey\" SET invitationCode = #{invitationCode}, state = #{state}, info = #{info}, creator_id = #{creatorId} WHERE id = #{id}")
-//    void updateSurvey(SurveyEntity survey);
-//
-//    @Delete("DELETE FROM \"Survey\" WHERE id = #{id}")
-//    void deleteSurvey(int id);
-//}
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.sustech.groupup.entity.db.SurveyEntity;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+import java.util.List;
+
+@Mapper
+public interface SurveyMapper extends BaseMapper<SurveyEntity> {
+
+    @Select("select survey_owner.owner_id from survey_owner where survey_id= #{surveyId}")
+    List<Long> getOwnerIdBySurveyId (@Param("surveyId") Long SurveyId);
+
+    @Select("select survey_member.member_id from survey_member where survey_id= #{surveyId}")
+    List<Long> getMemberIdBySurveyId (@Param("surveyId") Long SurveyId);
+
+    @Update("update survey set status = #{newStatus} where id = #{surveyId}")
+    void updateStatusById(@Param("surveyId") Long SurveyId, @Param("newStatus") int newStatus);
+}

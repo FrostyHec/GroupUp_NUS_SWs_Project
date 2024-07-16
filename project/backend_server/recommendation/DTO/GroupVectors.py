a5db83f22eeb2e1b3bc09c5fca3d06fcd51af97b
Frosty <112929809+FrostyHec@users.noreply.github.com>
2024-07-16 20:59:31 +0800

    obj-push

--- project/backend_server/recommendation/DTO/GroupVectors.py
+++ project/backend_server/recommendation/DTO/GroupVectors.py
@@ -5,5 +5,9 @@ from algorithm.Vector import Vector
 
 class GroupVectors:
     def __init__(self, group_id: int, group_info: Dict[int, Vector]):
+        """
+        :param group_id: group_id
+        :param group_info: <userid,vector>
+        """
         self.group_id: int = group_id
         self.info: Dict[int, Vector] = group_info
--- project/backend_server/recommendation/DTO/Recommender.py
+++ project/backend_server/recommendation/DTO/Recommender.py
@@ -1,3 +1,4 @@
+import json
 from typing import List, Dict, Optional
 
 from DTO.GroupVectors import GroupVectors
@@ -9,26 +10,52 @@ class Recommender:
     def __init__(self):
         pass
 
-    def get_vector(self, survey, answer) -> Vector:
+    def get_vector(self, survey: json, answer: json) -> Vector:
+        """
+        :param survey: 标准问卷的json格式
+        :param answer:
+        :return:
+        """
         pass
 
-    def get_willing(self, survey, willing: str) -> Vector:
+    def get_willing(self, survey: json, willing: str) -> Vector:
         pass
 
     def get_person_preference_order(self, user: Vector,
                                     user_group: Optional[GroupVectors],
                                     current_ungrouped_person: Dict[int, Vector],
-                                    restriction) -> Dict[int, float]:
+                                    restriction: json) -> Dict[int, float]:
+        """
+        :param user: 用户的向量
+        :param user_group: 可能用户不在一个组中，此时为None
+        :param current_ungrouped_person: 未组队的个人 <userid,vector>
+        :param restriction: 组队的限制
+        :return: <userid,recommendation_value(0~100)>
+        """
         pass
 
     def get_group_preference_order(self, user: Vector,
                                    user_group: Optional[GroupVectors],
                                    current_group: List[GroupVectors],
-                                   restriction
+                                   restriction: json
                                    ) -> Dict[int, float]:
+        """
+        :param user: 用户的向量
+        :param user_group: 可能用户不在一个组中，此时为None
+        :param current_group: 当前的组队的组
+        :param restriction: 组队的限制
+        :return: <groupid,recommendation_value(0~100)>
+        """
         pass
 
     def grouping(self, current_group: List[GroupVectors],
                  current_ungrouped: Dict[int, Vector],
-                 restriction) -> List[GroupVectors]:
+                 restriction: json
+                 ) -> List[GroupVectors]:
+        """
+        :param current_group:
+        :param current_ungrouped:
+        :param restriction:
+        :return:
+        """
         pass

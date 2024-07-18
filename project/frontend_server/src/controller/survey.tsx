9e0be3ceaf146b79607278feb312e35863106c7d
Jaredan <Jaredanjerry@outlook.com>
2024-07-18 19:26:11 +0800

    Test npm run build

--- project/frontend_server/src/controller/survey.tsx
+++
@@ -1,8 +0,0 @@
-import { surveyInfo } from "@/actions/survey";
-
-export function isUserOwner({ id }: { id: number }) {
-  const survey = surveyInfo({ surveyID: id });
-  const ownerIDs = survey.data.data.owners;
-  const isOwner = ownerIDs.includes(id);
-  return isOwner;
-}
